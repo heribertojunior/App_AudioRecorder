296b514c6d43040dbe6c57446daf2733e7f0422c
Heriberto Junior <hsc.jr94@gmail.com>
2019-04-08 19:53:01 -0400

    first commit

---
+++ App7/App7/Consumo.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using Plugin.SimpleAudioPlayer;
+using Plugin.AudioRecorder;
+using System.Timers;
+using System.Reflection;
+using System.Collections;
+
+namespace App7
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Consumo : ContentPage
+	{
+        AudioPlayer player;
+        ArrayList listaArquiv = new ArrayList();
+        public Consumo ()
+		{
+            InitializeComponent();
+            NavigationPage.SetHasBackButton(this, false);
+            //listaArquivo.ItemsSource = lista;
+        }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+
+        protected override void OnAppearing()
+        {
+           // var meuarquivo = new List<Arquivo>();
+
+            var arquivos = Directory.EnumerateFiles(App.PastaDiretorio, "*.wav");
+            foreach (var nomearquivo in arquivos)
+            {
+                //File.Delete(nomearquivo);
+                int indice = nomearquivo.LastIndexOf('/');
+                listaArquiv.Add(nomearquivo.Substring(indice + 1));
+               // meuarquivo.Add(listaArquiv);
+                
+               
+            }
+            listaArquivo.ItemsSource = listaArquiv;
+            //listaArquivo.ItemsSource = App.listaRecorder;
+        }
+
+        async void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            //var valor = sender;
+            //var eh = e;
+
+            bool delet = await DisplayAlert("Ouvir", "Dar Play?" + e.ToString() + "?", "OK", "Cancelar");
+
+            if (delet)
+            {
+                string nomeArq = Path.Combine(App.PastaDiretorio, $"{e.SelectedItem.ToString()}");
+                player.Play(nomeArq);
+
+                //await Navigation.PopAsync();
+                await Navigation.PushAsync(new Consumo());
+            }
+        }
+
+    }
+}
---
+++ App7/App7/PageConf.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using Plugin.SimpleAudioPlayer;
+using Plugin.AudioRecorder;
+using System.Timers;
+using System.Reflection;
+
+
+namespace App7
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageConf : ContentPage
+	{
+        AudioRecorderService recorder;
+        AudioPlayer player;
+        bool isTimerRunning = false;
+        int seconds = 0, minutes = 0;
+        string nome;
+
+        public PageConf ()
+		{
+			InitializeComponent ();
+            if (Device.RuntimePlatform == Device.iOS)
+                this.Padding = new Thickness(0, 28, 0, 0);
+
+            recorder = new AudioRecorderService
+            {
+                StopRecordingAfterTimeout = true,
+                TotalAudioTimeout = TimeSpan.FromSeconds(15),
+                AudioSilenceTimeout = TimeSpan.FromSeconds(2)
+            };
+
+            player = new AudioPlayer();
+            player.FinishedPlaying += Finish_Playing;
+
+        }
+        void Finish_Playing(object sender, EventArgs e)
+        {
+            bntRecord.IsEnabled = true;
+            bntRecord.BackgroundColor = Color.FromHex("#7cbb45");
+            bntPlay.IsEnabled = true;
+            bntPlay.BackgroundColor = Color.FromHex("#7cbb45");
+            bntStop.IsEnabled = false;
+            bntStop.BackgroundColor = Color.Silver;
+            lblSeconds.Text = "00";
+            lblMinutes.Text = "00";
+        }
+
+        async void Record_Clicked(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if (!recorder.IsRecording)
+            {
+                seconds = 0;
+                minutes = 0;
+                isTimerRunning = true;
+                Device.StartTimer(TimeSpan.FromSeconds(1), () => {
+
+
+                    if (seconds.ToString().Length == 1)
+                    {
+                        lblSeconds.Text = "0" + seconds.ToString();
+                    }
+                    else
+                    {
+                        lblSeconds.Text = seconds.ToString();
+                    }
+                    if (seconds == 60)
+                    {
+                        minutes++;
+                        seconds = 0;
+
+                        if (minutes.ToString().Length == 1)
+                        {
+                            lblMinutes.Text = "0" + minutes.ToString();
+                        }
+                        else
+                        {
+                            lblMinutes.Text = minutes.ToString();
+                        }
+
+                        lblSeconds.Text = "00";
+                    }
+                    return isTimerRunning;
+                });
+
+                recorder.StopRecordingOnSilence = IsSilence.IsToggled;
+                var audioRecordTask = await recorder.StartRecording();
+
+                bntRecord.IsEnabled = false;
+                bntRecord.BackgroundColor = Color.Silver;
+                bntPlay.IsEnabled = true;
+                bntPlay.BackgroundColor = Color.Silver;
+                bntStop.IsEnabled = true;
+                bntStop.BackgroundColor = Color.FromHex("#7cbb45");
+
+
+            }
+        }
+
+        async void Stop_Clicked(object sender, EventArgs e)
+        {
+            StopRecording();
+            await recorder.StopRecording();
+
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        void StopRecording()
+        {
+
+            isTimerRunning = false;
+            bntRecord.IsEnabled = true;
+            bntRecord.BackgroundColor = Color.FromHex("#7cbb45");
+            bntPlay.IsEnabled = true;
+            bntPlay.BackgroundColor = Color.FromHex("#7cbb45");
+            bntStop.IsEnabled = false;
+            bntStop.BackgroundColor = Color.Silver;
+            lblSeconds.Text = "00";
+            lblMinutes.Text = "00";
+
+
+
+        }
+        void Play_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var FilePath = App.PastaDiretorio + "/ARS_" + nome + ".wav";
+                StopRecording();
+                player.Play(FilePath);
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                nome = picker.Items[selectedIndex];
+            }
+            var FilePath = App.PastaDiretorio + "/ARS_" + nome + ".wav";
+            teste_lbl.Text = FilePath;
+        }
+    }
+}
